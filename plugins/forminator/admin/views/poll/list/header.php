c02f7d8949be6a5d523f632c1a8f49b8fc3e10bd
Yufan Wu <wu000284@algonquinlive.com>
2023-07-05 23:46:15 -0400

    initial version

--- plugins/forminator/admin/views/poll/list/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-$title         = esc_html__( 'Polls', 'forminator' );
-$create_dialog = 'polls';
-$import_dialog = 'import_poll';
-$hash          = '#polls';
-
-require_once forminator_plugin_dir() . 'admin/views/common/list/header.php';
